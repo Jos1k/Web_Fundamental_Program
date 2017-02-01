4437c3e586ba325707465247adf76c2dbae1b094
Ihor Hadzera <josik4ke@gmail.com>
2017-02-01 23:51:34 +0100

    fixed if statement

--- task_06_jsCore/app.js
+++ task_06_jsCore/app.js
@@ -1,29 +1,28 @@
 var polishNotation = function(expression, extraOperators){
-    var allowedOperators = '+-*';
-    var elementStack = [];
-    var result = 0;
     if (expression.match(/[A-Za-z\.\,]/i)){
         throw new Error('Invalid expression! Not supported characters!');
     }
     expression = expression.replace(/\s+/g,' ').trim();
+    
     var expressionElements = expression.split(' ');
+    var allowedOperators = ['+','-','*'].concat(extraOperators);
+    var elementStack = [];
 
     expressionElements.forEach(function(element) {
         var parsedElem = parseInt(element,10);
-        if (!isNaN (parsedElem )){
-            elementStack.push(parsedElem);
+        if (Number.isInteger(parsedElem)){
+            elementStack.push(element);
         }
         else {
             var lastValue = elementStack.pop();
             var firstValue = elementStack.pop();
-            if (allowedOperators.indexOf(element)!= -1 
-                || (extraOperators && extraOperators.indexOf(element)!=-1)){
-                        return elementStack.push(eval(firstValue + element + lastValue));
-                }
+            if (allowedOperators.indexOf(element)!= -1){
+                return elementStack.push(eval(firstValue + element + lastValue));
+            }
             throw new Error('Invalid expression! Not supported operators!');
         }
     });
-    if (elementStack.length === 1 && !isNaN(elementStack[0])){
+    if (elementStack.length === 1 && Number.isInteger(elementStack[0])){
         return elementStack.pop();
     }
     throw new Error('Invalid expression! Incorrect count values or operators!')
