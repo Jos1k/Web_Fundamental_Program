80f1b029f80dbde63ecebee6086914e2fdff7bbb
Ihor Hadzera <josik4ke@gmail.com>
2017-02-01 23:56:18 +0100

    minor changes

--- task_06_jsCore/app.js
+++ task_06_jsCore/app.js
@@ -1,31 +1,26 @@
 var polishNotation = function(expression, extraOperators){
-    if (expression.match(/[A-Za-z\.\,]/i)){
+    if (expression.match(/[A-Za-z\.\,]/i))
         throw new Error('Invalid expression! Not supported characters!');
-    }
+
     expression = expression.replace(/\s+/g,' ').trim();
-    
     var expressionElements = expression.split(' ');
     var allowedOperators = ['+','-','*'].concat(extraOperators);
     var elementStack = [];
 
     expressionElements.forEach(function(element) {
         var parsedElem = parseInt(element,10);
-        if (Number.isInteger(parsedElem)){
-            elementStack.push(element);
-        }
+        if (Number.isInteger(parsedElem))
+            elementStack.push(parsedElem);
         else {
             var lastValue = elementStack.pop();
             var firstValue = elementStack.pop();
-            if (allowedOperators.indexOf(element)!= -1){
+            if (allowedOperators.indexOf(element)!= -1)
                 return elementStack.push(eval(firstValue + element + lastValue));
-            }
             throw new Error('Invalid expression! Not supported operators!');
         }
     });
-    if (elementStack.length === 1 && !isNaN(elementStack[0])){
+    if (elementStack.length === 1 && !isNaN(elementStack[0]))
         return elementStack.pop();
-    }
     throw new Error('Invalid expression! Incorrect count values or operators!')
 };
-
 module.exports.processExpression = polishNotation;
