3ecc94e492af03c8e0d53b278c3d900aea2cdec3
Ihor Hadzera <ihor_hadzera@epam.com>
2017-02-26 22:11:31 +0100

    wrapped to func

--- task_09_JSOOP/wd.js
+++ task_09_JSOOP/wd.js
@@ -1,32 +1,35 @@
 var wd = {
     createRoot: function (appName) {
-        const that = this;
-        let appRoutes = [];
-        const applicationName = appName;
-        const content_area = document.querySelector('[wd-root]', applicationName);
-        const getContent = (templateUri) => {
-            return fetch(templateUri).then(response => {
-                return response.text().then(text => {
-                    return text;
-                })
-            }).catch(ex => console.log(ex));
-        };
+        function AppInstanse(name) {
+            const that = this;
+            let appRoutes = [];
+            const applicationName = appName;
+            const content_area = document.querySelector('[wd-root]', applicationName);
+            const getContent = (templateUri) => {
+                return fetch(templateUri).then(response => {
+                    return response.text().then(text => {
+                        return text;
+                    })
+                }).catch(ex => console.log(ex));
+            };
 
-        this.root = () =>{ return content_area }
-        this.routes = (routeList)  => {
-            appRoutes = routeList;
+            this.root = () => { return content_area }
+            this.routes = (routeList) => {
+                appRoutes = routeList;
+                return that;
+            };
+
+            this.navigate = (templateUrl) => {
+                const route = appRoutes.find(element => {
+                    return element.url === templateUrl;
+                });
+                return getContent(route.templateUrl).then(content => {
+                    content_area.innerHTML = content
+                    history.pushState(content, null, route.url);
+                }).catch(ex => console.log(ex));
+            };
             return that;
-        };
-
-        this.navigate = (templateUrl) => {
-            const route = appRoutes.find(element => {
-                return element.url === templateUrl;
-            });
-            return getContent(route.templateUrl).then(content => {
-                content_area.innerHTML = content
-                history.pushState(content, null, route.url);
-            }).catch(ex => console.log(ex));
-        };
-        return that;
+        }
+        return new AppInstanse(appName);
     }
 };
