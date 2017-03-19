cb8421558d5286983fa05804a2dd49fc5dedbaca
Ihor Hadzera <ihor_hadzera@epam.com>
2017-03-19 13:20:05 +0100

    replaced content folder; changed quotes

--- task_12_webpack/webpack.config.js
+++ task_12_webpack/webpack.config.js
@@ -1,21 +1,22 @@
+var debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractSASS = new ExtractTextPlugin('style.css');
 
 module.exports = {
-    context: __dirname + "/static",
-    devtool: "inline-sourcemap",
-    entry: "./scripts/app.js",
+    context: __dirname + '/static',
+    devtool: 'inline-sourcemap',
+    entry: './scripts/app.js',
     output: {
-        path: __dirname + "/static",
-        filename: "scripts.js"
+        path: __dirname + '/static',
+        filename: 'scripts.js'
     },
     plugins: [extractSASS],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: [/libs/],
+                include: [/scripts/],
                 enforce:'pre',
                 use:[{loader:'eslint-loader'}]
             },
