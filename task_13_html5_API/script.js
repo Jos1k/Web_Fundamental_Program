f06e20a5bc6db4481c3e4d788b0f7b4895eaee01
Ihor Hadzera <ihor_hadzera@epam.com>
2017-04-02 20:13:49 +0200

    separated mode

--- task_13_html5_API/script.js
+++ task_13_html5_API/script.js
@@ -1,27 +1,37 @@
 window.onload = function () {
-    var currentRow, currentCell, player, actionCount;
-    var gameTable = document.getElementsByClassName('main-content_game_area')[0].rows;
+    var currentRow = 1;
+    var currentCell = 1;
+    var player = 'X';
+    var actionCount = 0;
+    var boardSize = 3;
+    var currentMode = {};
     var textBlock = document.getElementsByClassName('main-content_text-block')[0];
-    var gameTableAray = [];
 
-    function initGameTable() {
-        var storageBoardItem = localStorage.getItem('gameBoard');
-        if (storageBoardItem) {
-            gameTableAray = JSON.parse(storageBoardItem);
-            for (let x = 0; x < gameTable.length; x++) {
-                for (let y = 0; y < gameTable.length; y++) {
-                    gameTable[x].cells[y].textContent = gameTableAray[x][y];
-                }
+    function loadGameBoard() {
+        let gameTableAray= JSON.parse(localStorage.getItem('gameBoard'));
+        let lastAct = localStorage.getItem('lastAct');
+        if (lastAct){
+            if (lastAct == 'X'){
+                player = 'O';
             }
-            return;
+            else{
+                player = 'X';
+            }
         }
-        else {
-            for (let x = 0; x < gameTable.length; x++) {
+        if (!gameTableAray) {
+            gameTableAray = [];
+            for (let x = 0; x < boardSize; x++) {
                 gameTableAray[x] = [];
-                for (let y = 0; y < gameTable.length; y++) {
-                    gameTableAray[x][y] = gameTable[x].cells[y].textContent;
+                for (let y = 0; y < boardSize; y++) {
+                    gameTableAray[x][y] = '';
                 }
             }
+        }
+        else{
+            actionCount = gameTableAray.filter((el)=> el !== '').length;
+        }
+        if (!localStorage.getItem('isCanvasMode')) {
+            currentMode = new TextRenderMode(gameTableAray);
         }
     }
 
@@ -30,50 +40,44 @@ window.onload = function () {
         currentCell = 1;
         player = 'X';
         actionCount = 0;
-        for (let i = 0; i < gameTable.length; i++) {
-            for (let j = 0; j < gameTable[i].cells.length; j++) {
-                gameTable[i].cells[j].textContent = '-';
-                gameTable[i].cells[j].classList.remove('selectedElement');
-            }
-        }
-        gameTable[currentRow].cells[currentCell].classList.add('selectedElement');
+        localStorage.removeItem('gameBoard');
+        currentMode.resetBoard();
     }
 
     function isWinnerDetermined() {
-        var matrixSize = gameTable.length;
         if (currentRow == currentCell) {
-            for (let i = 0; i < matrixSize; i++) {
-                if (gameTable[i].cells[i].textContent != player)
+            for (let i = 0; i < boardSize; i++) {
+                if (currentMode.getValue(i, i) != player)
                     break;
-                if (i == matrixSize - 1) {
+                if (i == boardSize - 1) {
                     localStorage.clear();
                     return true;
                 }
             }
         }
-        if (currentRow + currentCell == matrixSize - 1) {
-            for (let i = 0; i < matrixSize; i++) {
-                if (gameTable[i].cells[(matrixSize - 1) - i].textContent != player)
+        if (currentRow + currentCell == boardSize - 1) {
+            for (let i = 0; i < boardSize; i++) {
+                if (currentMode.getValue(i, (boardSize - 1)-i) != player)
                     break;
-                if (i == matrixSize - 1) {
+                if (i == boardSize - 1) {
                     localStorage.clear();
                     return true;
                 }
             }
         }
-        for (let i = 0; i < matrixSize; i++) {
-            if (gameTable[currentRow].cells[i].textContent != player)
+        for (let i = 0; i < boardSize; i++) {
+            if (currentMode.getValue(currentRow,i) != player)
                 break;
-            if (i == matrixSize - 1) {
+            if (i == boardSize - 1) {
                 localStorage.clear();
                 return true;
             }
         }
 
-        for (let i = 0; i < matrixSize; i++) {
-            if (gameTable[i].cells[currentCell].textContent != player)
+        for (let i = 0; i < boardSize; i++) {
+            if (currentMode.getValue(i,currentCell) != player)
                 break;
-            if (i == matrixSize - 1) {
+            if (i == boardSize - 1) {
                 localStorage.clear();
                 return true;
             }
@@ -83,18 +87,16 @@ window.onload = function () {
 
     function actionMove() {
         if (event.keyCode == 32) {
-            let currentElement = gameTable[currentRow].cells[currentCell];
-            if (currentElement.textContent != 'X' && currentElement.textContent != 'O') {
-                currentElement.textContent = player;
-                gameTableAray[currentRow][currentCell] = currentElement.textContent;
-                localStorage.setItem('gameBoard', JSON.stringify(gameTableAray));
+            let currentElement = currentMode.getValue(currentRow, currentCell);
+            if (currentElement != 'X' && currentElement != 'O') {
+                currentMode.act(currentRow, currentCell,player);
                 actionCount++;
                 if (isWinnerDetermined()) {
                     alert('Player ' + player + ' Wins!');
                     resetGameBoard();
                 }
                 else {
-                    if (actionCount == gameTable.length * gameTable.length) {
+                    if (actionCount == boardSize * boardSize) {
                         alert('Draw!');
                         resetGameBoard();
                         return;
@@ -109,7 +111,8 @@ window.onload = function () {
                 }
             }
         } else if ([37, 38, 39, 40].indexOf(event.keyCode) >= 0) {
-            gameTable[currentRow].cells[currentCell].classList.remove('selectedElement');
+            let oldRow = currentRow;
+            let oldCell = currentCell;
             switch (event.keyCode) {
                 case 38:
                     if (currentRow > 0) {
@@ -117,7 +120,7 @@ window.onload = function () {
                     }
                     break;
                 case 40:
-                    if (currentRow < gameTable.length - 1) {
+                    if (currentRow < boardSize) {
                         currentRow++;
                     }
                     break;
@@ -127,21 +130,63 @@ window.onload = function () {
                     }
                     break;
                 case 39:
-                    if (currentCell < gameTable.length - 1) {
+                    if (currentCell < boardSize) {
                         currentCell++;
                     }
                     break;
             }
-            gameTable[currentRow].cells[currentCell].classList.add('selectedElement');
+            currentMode.moveCursor(oldRow, oldCell, currentRow, currentCell);
         }
     }
 
-    resetGameBoard();
-    initGameTable();    
-    
+    function setMode(elem) {
+        localStorage.setItem("isCanvasMode", elem);
+    }
+
+    loadGameBoard();
+
     document.onkeydown = actionMove;
-    document.getElementById('reset-button').addEventListener("click",()=>{
-        localStorage.removeItem('gameBoard');
+    document.getElementById('reset-button').addEventListener("click", () => {
         resetGameBoard();
     });
 };
+
+class TextRenderMode {
+
+    constructor(gameBoard) {
+        this.gameBoard = gameBoard;
+        this.gameTable = document.getElementsByClassName('main-content_game_area')[0].rows;
+        this.pivotElemIndex = Math.floor(this.gameBoard.length / 2);
+
+        for (let x = 0; x < this.gameBoard.length; x++) {
+            for (let y = 0; y < this.gameBoard.length; y++) {
+                this.gameTable[x].cells[y].textContent = gameBoard[x][y] ? gameBoard[x][y] : '-';
+            }
+        }
+        this.gameTable[this.pivotElemIndex].cells[this.pivotElemIndex].classList.add('selectedElement');
+    }
+
+    resetBoard() {
+        for (let i = 0; i < this.gameBoard.length; i++) {
+            for (let j = 0; j < this.gameBoard.length; j++) {
+                this.gameTable[i].cells[j].textContent = '-';
+                this.gameTable[i].cells[j].classList.remove('selectedElement');
+            }
+        }
+        this.gameTable[this.pivotElemIndex].cells[this.pivotElemIndex].classList.add('selectedElement');
+    }
+
+    moveCursor(oldRow, oldCell, newRow, newCell) {
+        this.gameTable[oldRow].cells[oldCell].classList.remove('selectedElement');
+        this.gameTable[newRow].cells[newCell].classList.add('selectedElement');
+    }
+    act(row, cell, player) {
+        this.gameTable[row].cells[cell].textContent = player;
+        this.gameBoard[row][cell] = player;
+        localStorage.setItem('gameBoard', JSON.stringify(this.gameBoard));
+        localStorage.setItem('lastAct',player);
+    }
+    getValue(row, cell) {
+        return this.gameTable[row].cells[cell].textContent;
+    }
+}
